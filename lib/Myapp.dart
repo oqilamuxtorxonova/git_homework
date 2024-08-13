2bdbf8c5832ab76b1e9fbea55ad293775e848d54
Пользователь <inomov.id@gmail.com>
2024-08-13 17:37:58 +0300

    Prayektga pages qoshildi

--- lib/Myapp.dart
+++ lib/Myapp.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:git_homework/pages/pages_1_.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Pages1(),
+    );
   }
 }
---
+++ lib/pages/pages_2_.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Pages2 extends StatefulWidget {
+  const Pages2({super.key});
+
+  @override
+  State<Pages2> createState() => _Pages2State();
+}
+
+class _Pages2State extends State<Pages2> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
