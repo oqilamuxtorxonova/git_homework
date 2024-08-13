8f074207e7de3407dc0de133c98803f0438af145
Пользователь <inomov.id@gmail.com>
2024-08-13 17:27:39 +0300

    Prayektga main bilan myapp file ochildi

---
+++ android/app/src/main/kotlin/dev/example/git_homework/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.example.git_homework
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:git_homework/Myapp.dart';
+
+void main() {
+
+  runApp(const MyApp());
+}
